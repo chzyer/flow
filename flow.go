05a81b6d2017c6079aea6350da882cffdebab351
chzyer <0@0xdf.com>
2016-04-26 10:31:12 +0800

    fix test

--- flow.go
+++ flow.go
@@ -53,6 +53,7 @@ func NewEx(n int) *Flow {
 		ref:      new(int32),
 	}
 	f.appendDebug("init")
+	f.Add(n)
 	return f
 }
 
--- flow_test.go
+++ flow_test.go
@@ -3,11 +3,51 @@ package flow
 import (
 	"sync/atomic"
 	"testing"
+	"time"
 )
+
+func TestStopWaitFork(t *testing.T) {
+	f := New()
+
+	done := make(chan struct{})
+
+	{ // worker
+		go func() {
+			f.Add(1)
+			defer f.DoneAndClose()
+			<-done
+		}()
+	}
+
+	go func() {
+		for _ = range time.Tick(time.Second) {
+			println(string(f.GetDebug()))
+			break
+		}
+	}()
+
+	go func() {
+		f.Stop()
+		{
+			f2 := f.Fork(0)
+			done2 := make(chan struct{})
+			go func() {
+				f2.Add(1)
+				<-done2
+				f2.Done()
+			}()
+			close(done2)
+			f2.Wait()
+		}
+		time.Sleep(100 * time.Millisecond)
+		close(done)
+	}()
+	f.Wait()
+}
 
 func TestFlow(t *testing.T) {
 	{
-		f := New(1)
+		f := NewEx(1)
 		done := 0
 		go func() {
 			defer f.Done()
@@ -23,7 +63,7 @@ func TestFlow(t *testing.T) {
 	}
 
 	{
-		f := New(2)
+		f := NewEx(2)
 		var done int64
 		go func() {
 			defer f.Done()
